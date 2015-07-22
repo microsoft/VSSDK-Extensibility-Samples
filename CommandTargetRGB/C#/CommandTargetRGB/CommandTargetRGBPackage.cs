a9b86174eabe34ad0f2828d5210c995ac20fcba3
Selma Ikiz <selmai@microsoft.com>
2015-07-22 16:07:31 -0700

    multi-instance tool window

---
+++ CommandTargetRGB/C#/CommandTargetRGB/CommandTargetRGBPackage.cs
@@ -0,0 +1,116 @@
+﻿/***************************************************************************
+ 
+Copyright (c) Microsoft Corporation. All rights reserved.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.CommandTargetRGB
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    // a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // This attribute is used to register the informations needed to show the this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    // This attribute is needed to let the shell know that this package exposes some menus.
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    // This attribute registers a tool window exposed by this package.
+    [ProvideToolWindow(typeof(RGBToolWindow), MultiInstances=true)]
+    [Guid(GuidList.guidCommandTargetRGBPkgString)]
+    public sealed class CommandTargetRGBPackage : Package
+    {
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public CommandTargetRGBPackage()
+        {
+        }
+
+        /// <summary>
+        /// This function is called when the user clicks the menu item that shows the 
+        /// tool window. See the Initialize method to see how the menu item is associated to 
+        /// this function using the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void ShowToolWindow(object sender, EventArgs e)
+        {
+            // For a multi-instance ToolWindow, find an unused ID
+            int id = FindUnusedToolWindowId(typeof(RGBToolWindow));
+
+            // Create the window with the unused ID.
+            var window = CreateToolWindow(typeof(RGBToolWindow), id) as RGBToolWindow;
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException(Resources.CanNotCreateWindow);
+            }
+
+            // Display the window.
+            IVsWindowFrame windowFrame = (IVsWindowFrame) window.Frame;
+            VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+        
+        /// <summary>
+        /// Find an unused ID for a new multi instance toolwindow.
+        /// </summary>
+        /// <param name="toolWindowType">The type of the toolwindow</param>
+        /// <returns>An unused ID</returns>
+        private int FindUnusedToolWindowId(Type toolWindowType)
+        {
+            for (int id = 0; ; ++id)
+            {
+                ToolWindowPane window = FindToolWindow(toolWindowType, id, false);
+                if (window == null)
+                {
+                    return id;
+                }
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that relies on services provided by Visual Studio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if ( null != mcs )
+            {
+                // Create the command for the tool window
+                CommandID toolwndCommandID = new CommandID(GuidList.guidCommandTargetRGBCmdSet, (int)PkgCmdIDList.cmdidShowToolWindow);
+                MenuCommand menuToolWin = new MenuCommand(ShowToolWindow, toolwndCommandID);
+                mcs.AddCommand(menuToolWin);
+            }
+        }
+        #endregion
+    }
+}
