7ae75c7c8dfa08eea56add87baaeadfb91172259
Jessie Zhang (CHINASOFT INTERNATIONAL INC) <v-zhajes@microsoft.com>
2017-04-19 00:24:08 -0700

    add error list sample

---
+++ ErrorList/C#/SpellCheckerProvider.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualStudio.Shell.TableControl;
+using Microsoft.VisualStudio.Shell.TableManager;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace SpellChecker
+{
+    /// <summary>
+    /// Factory for the <see cref="ITagger{T}"/>. There will be one instance of this class/VS session.
+    /// 
+    /// It is also the <see cref="ITableDataSource"/> that reports spelling errors in comments.
+    /// </summary>
+    [Export(typeof(IViewTaggerProvider))]
+    [TagType(typeof(IErrorTag))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TextViewRole(PredefinedTextViewRoles.Analyzable)]
+    internal sealed class SpellCheckerProvider : IViewTaggerProvider, ITableDataSource
+    {
+        internal readonly ITableManager ErrorTableManager;
+        internal readonly ITextDocumentFactoryService TextDocumentFactoryService;
+        internal readonly IClassifierAggregatorService ClassifierAggregatorService;
+
+        const string _spellCheckerDataSource = "SpellChecker";
+
+        private readonly List<SinkManager> _managers = new List<SinkManager>();      // Also used for locks
+        private readonly List<SpellChecker> _spellCheckers = new List<SpellChecker>();
+
+        [ImportingConstructor]
+        internal SpellCheckerProvider([Import]ITableManagerProvider provider, [Import] ITextDocumentFactoryService textDocumentFactoryService, [Import] IClassifierAggregatorService classifierAggregatorService)
+        {
+            this.ErrorTableManager = provider.GetTableManager(StandardTables.ErrorsTable);
+            this.TextDocumentFactoryService = textDocumentFactoryService;
+
+            this.ClassifierAggregatorService = classifierAggregatorService;
+
+            this.ErrorTableManager.AddSource(this, StandardTableColumnDefinitions.DetailsExpander, 
+                                                   StandardTableColumnDefinitions.ErrorSeverity, StandardTableColumnDefinitions.ErrorCode,
+                                                   StandardTableColumnDefinitions.ErrorSource, StandardTableColumnDefinitions.BuildTool,
+                                                   StandardTableColumnDefinitions.ErrorSource, StandardTableColumnDefinitions.ErrorCategory,
+                                                   StandardTableColumnDefinitions.Text, StandardTableColumnDefinitions.DocumentName, StandardTableColumnDefinitions.Line, StandardTableColumnDefinitions.Column);
+        }
+
+        /// <summary>
+        /// Create a tagger that does spell checking on the view/buffer combination.
+        /// </summary>
+        public ITagger<T> CreateTagger<T>(ITextView textView, ITextBuffer buffer) where T : ITag
+        {
+            ITagger<T> tagger = null;
+
+            // Only attempt to spell check on the view's edit buffer (and multiple views could have that buffer open simultaneously so
+            // only create one instance of the spell checker.
+            if ((buffer == textView.TextBuffer) && (typeof(T) == typeof(IErrorTag)))
+            {
+                var spellChecker = buffer.Properties.GetOrCreateSingletonProperty(typeof(SpellChecker), () => new SpellChecker(this, textView, buffer));
+
+                // This is a thin wrapper around the SpellChecker that can be disposed of without shutting down the SpellChecker
+                // (unless it was the last tagger on the spell checker).
+                tagger = new SpellCheckerTagger(spellChecker) as ITagger<T>;
+            }
+
+            return tagger;
+        }
+
+        #region ITableDataSource members
+        public string DisplayName
+        {
+            get
+            {
+                // This string should, in general, be localized since it is what would be displayed in any UI that lets the end user pick
+                // which ITableDataSources should be subscribed to by an instance of the table control. It really isn't needed for the error
+                // list however because it autosubscribes to all the ITableDataSources.
+                return "Spell Checker";
+            }
+        }
+
+        public string Identifier
+        {
+            get
+            {
+                return _spellCheckerDataSource;
+            }
+        }
+
+        public string SourceTypeIdentifier
+        {
+            get
+            {
+                return StandardTableDataSources.ErrorTableDataSource;
+            }
+        }
+
+        public IDisposable Subscribe(ITableDataSink sink)
+        {
+            // This method is called to each consumer interested in errors. In general, there will be only a single consumer (the error list tool window)
+            // but it is always possible for 3rd parties to write code that will want to subscribe.
+            return new SinkManager(this, sink);
+        }
+        #endregion
+
+        public void AddSinkManager(SinkManager manager)
+        {
+            // This call can, in theory, happen from any thread so be appropriately thread safe.
+            // In practice, it will probably be called only once from the UI thread (by the error list tool window).
+            lock (_managers)
+            {
+                _managers.Add(manager);
+
+                // Add the pre-existing spell checkers to the manager.
+                foreach (var spellChecker in _spellCheckers)
+                {
+                    manager.AddSpellChecker(spellChecker);
+                }
+            }
+        }
+
+        public void RemoveSinkManager(SinkManager manager)
+        {
+            // This call can, in theory, happen from any thread so be appropriately thread safe.
+            // In practice, it will probably be called only once from the UI thread (by the error list tool window).
+            lock (_managers)
+            {
+                _managers.Remove(manager);
+            }
+        }
+
+        public void AddSpellChecker(SpellChecker spellChecker)
+        {
+            // This call will always happen on the UI thread (it is a side-effect of adding or removing the 1st/last tagger).
+            lock (_managers)
+            {
+                _spellCheckers.Add(spellChecker);
+
+                // Tell the preexisting managers about the new spell checker
+                foreach (var manager in _managers)
+                {
+                    manager.AddSpellChecker(spellChecker);
+                }
+            }
+        }
+
+        public void RemoveSpellChecker(SpellChecker spellChecker)
+        {
+            // This call will always happen on the UI thread (it is a side-effect of adding or removing the 1st/last tagger).
+            lock (_managers)
+            {
+                _spellCheckers.Remove(spellChecker);
+
+                foreach (var manager in _managers)
+                {
+                    manager.RemoveSpellChecker(spellChecker);
+                }
+            }
+        }
+
+        public void UpdateAllSinks()
+        {
+            lock (_managers)
+            {
+                foreach (var manager in _managers)
+                {
+                    manager.UpdateSink();
+                }
+            }
+        }
+    }
+}
---
+++ ErrorList/Readme.md
@@ -0,0 +1,62 @@
+
+# ErrorList Sample
+A sample of a VSIX that generates errors in error list when you make spelling errors in the editor.
+
+* Technologies: Visual Studio 2017 SDK
+* Topics: Visual Studio Editor, VSX
+
+**Description**
+
+This sample generates the Messages in error list when you make spelling errors in the text that you type in the editor
+
+
+![image](CS/Example_Spellingerror.png)
+
+**Requirements**
+
+[ Visual Studio 2015 ](https://www.visualstudio.com/products/visual-studio-community-vs?wt.mc_id=o~display~github~vssdk)
+
+
+
+**Get all samples**
+
+Clone the repo ([How to](https://git-scm.com/book/en/v2/Git-Basics-Getting-a-Git-Repository#Cloning-an-Existing-Repository)):
+
+`git clone https://github.com/Microsoft/VSSDK-Extensibility-Samples.git`
+
+**Run the sample**
+
+  1. To run the sample, hit F5 or choose the **Debug &gt; Start Debugging** menu command. A new instance of Visual Studio will launch under the experimental hive. 
+  2. Once loaded, create new project (e.g. a C# ConsoleApplication) 
+  3. Right-click the project in the **File Explorer**. Click **Add &gt; New File**
+  4. In the **Add New File** dialog, select **Text File** from the list of options. 
+  5. Change the suggested filename to have 'TBX' as a file extension e.g. _TextFile1.tbx_. 
+  6. Click **Add** to add the new file to your project and close the dialog box 
+  7. The VSPackage opens a new file tab with the embedded rich textbox. 
+  8. Click **View &gt; Toolbox** to launch the Toolbox window. The window contains a **Toolbox Test** tab. As with all toolbox tabs, it has a Pointer tool. Drag the **Toolbox Sample Item** tool into the .tbx file window. 
+  9. **NOTE**: If your toolbox does not have a **Toolbox Test** tab or **Toolbox Sample Item** tool, make sure you have a .tbx file as the active file. The tab and tool are registered specifically to the .tbx file editor. 
+
+
+
+**Source Code Overview**
+
+This sample implements an editor for a.tbx** plain text file. The actual UI
+of this designer is simply a **RichTextBox** control. This sample demonstrates
+how to implement integration with Visual Studio Toolbox within the Editor.
+
+The Editor uses **SVsToolbox** service and implements **IVsToolboxUser** in
+order to support Toolbox integration. It handles toolbox items availability
+and supports drag and drop text from the toolbox.
+
+
+
+**Related topics**
+
+* [ Toolbox Documentation ](https://msdn.microsoft.com/en-us/library/ee712574(v=vs.140).aspx)
+
+* [ Editor Documentation ](https://msdn.microsoft.com/en-us/library/dd885242(v=vs.140).aspx)
+
+* [ Visual Studio SDK Documentation ](https://msdn.microsoft.com/en-us/library/bb166441(v=vs.140).aspx)
+
+
+
