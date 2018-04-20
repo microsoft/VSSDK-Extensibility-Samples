1a99124611c2a3cff8c3c487d2e721e746268c13
Oleg Tkachenko <olegtk@microsoft.com>
2018-04-20 13:18:50 -0700

    initial commit

---
+++ Backwards_Compatible_Editor_Command/src/CommandDefinition/JoinLinesCommandPackage.cs
@@ -0,0 +1,28 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace CommandDefinition
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(JoinLinesCommandPackage.PackageGuidString)]
+    [ProvideBindingPath]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class JoinLinesCommandPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "552a4344-959e-4e69-b7d8-52c3e6b76819";
+    }
+}
---
+++ Backwards_Compatible_Editor_Command/src/CommandImplementation/JoinLine.cs
@@ -0,0 +1,46 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using System;
+
+namespace JoinLineCommandImplementation
+{
+    public static class JoinLine
+    {
+        public static void JoinSelectedLines(ITextView textView)
+        {
+            if (textView.Selection.IsEmpty)
+            {
+                return;
+            }
+
+            var selectedSpan = textView.Selection.SelectedSpans[0];
+            textView.TextBuffer.Replace(selectedSpan, selectedSpan.GetText().Replace("\r\n", " "));
+
+            ThreadHelper.Generic.BeginInvoke(() =>
+            {
+                var shellCommandDispatcher = GetShellCommandDispatcher(textView);
+                Guid cmdGroup = VSConstants.VSStd2K;
+                shellCommandDispatcher.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.FORMATDOCUMENT,
+                    (uint)OLECMDEXECOPT.OLECMDEXECOPT_DODEFAULT, System.IntPtr.Zero, System.IntPtr.Zero);
+            });
+        }
+
+        private static IOleCommandTarget GetShellCommandDispatcher(ITextView view)
+        {
+            return ServiceProvider.GlobalProvider.GetService(typeof(SUIHostCommandDispatcher)) as IOleCommandTarget;
+        }
+    }
+}
