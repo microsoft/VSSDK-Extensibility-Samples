d5368197dbc1ee8d7eaf0930f850c03e592442fa
Adrian Vinca <adrianv@microsoft.com>
2019-07-22 15:27:34 -0700

    New Vsix Package project and added Custom Async Tool Window

---
+++ OperationProgress/OperationProgress/OperationProgressToolWindowControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace OperationProgress
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for OperationProgressToolWindowControl.
+    /// </summary>
+    public partial class OperationProgressToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OperationProgressToolWindowControl"/> class.
+        /// </summary>
+        public OperationProgressToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "OperationProgressToolWindow");
+        }
+    }
+}
